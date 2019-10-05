0d96a87805c3e3b49f525472378d9ab618a58b77
rafer1998 <raulfernandezsamc@gmail.com>
2019-10-05 16:58:35 +0200

    nothing

--- Practica2/src/java/buscarImagen.java
+++ Practica2/src/java/buscarImagen.java
@@ -10,7 +10,6 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.Cookie;
