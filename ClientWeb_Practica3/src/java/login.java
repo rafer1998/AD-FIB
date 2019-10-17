a8c24fa9b63e6eb2a4e957dd6557fd74853fd708
Ruben <ruben.bar.ar@gmail.com>
2019-10-17 17:35:38 +0200

    ClientWeb [registrarImagen]

--- ClientWeb_Practica3/src/java/login.java
+++ ClientWeb_Practica3/src/java/login.java
@@ -75,21 +75,7 @@ public class login extends HttpServlet {
                 response.sendRedirect("menu.jsp");
             }
             else
-                response.sendRedirect("error.jsp");
-            
-            
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet login</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet login at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-            
-            
+                response.sendRedirect("error.jsp");            
         }
          catch (Exception e) {
             System.err.println(e.getMessage());
