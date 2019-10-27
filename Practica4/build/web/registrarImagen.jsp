ff6c4f4f78d5087d3ae7e11aeb8973db9f1bc8b3
Ruben <ruben.bar.ar@gmail.com>
2019-10-27 09:57:48 +0100

    Funciones Principales

---
+++ Practica4/build/web/registrarImagen.jsp
@@ -0,0 +1,61 @@
+<%--
+	Document   : registrarImagen
+	Created on : 17-sep-2019, 15:44:12
+	Author 	: ruben.barcelo
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<%@page import="java.util.*" %>
+<!DOCTYPE html>
+<html>
+	<head>
+    	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    	<title>registImatge</title>
+        <style>
+            body {
+              background-color: lightblue;
+              text-align: center;
+            }            
+        </style>
+	</head>
+	<body>
+        <%
+        //Comprobacion usuario con sesion iniciada//
+        
+        String autor = "admin";   
+        
+        /*
+        HttpSession misession= (HttpSession) request.getSession();
+        autor = (String) misession.getAttribute("autor");
+        if(autor == null)
+               //Si el usuario no tiene sesion ->  redirect a login//
+               response.sendRedirect("login.jsp");
+        */
+        %>   
+            
+            
+    	<h1><i>Registrar Imagen</i></h1>
+   	 
+        <form action="./webresources/generic/register" method="POST">
+        	<p>Titulo: <input type="text" name="title" required></p>
+        	<p>Descripcion: <input type="text" name="description" required></p>
+        	<p>Palabras clave: <input type="text" name="keywords" placeholder="Palabras separadas con ';'" required></p>        	
+                <p><label for="date">Fecha creación:</label>
+                    <input type="date" name="creation" value="2010-12-16; " required></p>
+                <p><label for="avatar">Seleccionar imagen:</label>
+                    <input type="file" id="fichero" name="fichero" accept="image/png, image/jpeg" required></p>                
+        	<p>
+                <input type="hidden" name="author" value ="<%=autor%>"> 
+                
+                
+            	<input type="submit" value="Enviar">
+            	<input type="reset" value="Borrar">
+        	</p>
+    	</form>
+        
+        <form action="menu.jsp" method="POST" >        	
+            	<input type="submit" value="Menu">
+    	</form>
+   	 
+	</body>
+</html>
--- Practica4/src/java/practica4/GenericResource.java
+++ Practica4/src/java/practica4/GenericResource.java
@@ -5,6 +5,14 @@
  */
 package practica4;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.Produces;
@@ -51,14 +59,71 @@ public class GenericResource {
             @FormParam("keywords") String keywords,
             @FormParam("author") String author,
             @FormParam("creation") String crea_date){
-        return null;
+        Connection connection = null;
+        try {           
+            PreparedStatement statement;
+            String query;
+            
+            Class.forName("org.apache.derby.jdbc.ClientDriver");
+            connection = DriverManager.getConnection("jdbc:derby://localhost:1527/pr2;user=pr2;password=pr2"); 
+            
+            //id
+            int id = 0;
+            query = "select max(id) from imagen";
+            statement = connection.prepareStatement(query);
+            ResultSet rs = statement.executeQuery();
+            if (rs.next()) {
+                //Obtener siguiente id
+                id = rs.getInt(1)+1;
+            }  
+            
+            //Fecha alta
+            Date date = new Date();
+            DateFormat dateformat = new SimpleDateFormat("yyyy-MM-dd");
+            String altaDate = dateformat.format(date);
+            
+            try{
+                query = "insert into imagen values(?,?,?,?,?,?,?,?)";
+                statement = connection.prepareStatement(query);
+                
+                statement.setInt(1, id);
+                statement.setString(2, title); 
+                statement.setString(3, description);
+                statement.setString(4, keywords);
+                statement.setString(5, author);
+                statement.setString(6, crea_date);
+                statement.setString(7, altaDate);
+                statement.setString(8, title);
+                
+                statement.executeUpdate();                
+            }
+            catch(Exception e){
+                System.err.println(e.getMessage());
+                return "<h1>Imagen no se ha podido subir</h1>";
+            }
+        } 
+        catch (Exception e) {
+            System.err.println(e.getMessage());
+        } 
+        finally {
+            try {
+                if (connection != null)
+                    connection.close();               
+            } 
+            catch (Exception e) {
+                // connection close failed.
+                System.err.println(e.getMessage());
+                return "<h1>Imagen no se ha podido subir</h1>";
+            }
+        }
+        return "<h1>Imagen subida correctamente</h1>";
     }
     /**
  * POST method to register a new image
  * @param title
  * @param description
  * @param keywords
- * @param author
+ * @param id
  * @param crea_date
  * @return
  */
@@ -67,11 +132,58 @@ public class GenericResource {
  @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
  @Produces(MediaType.TEXT_HTML)
  public String modifyImage (@FormParam("title") String title,
-    @FormParam("description") String description,
-    @FormParam("keywords") String keywords,
-    @FormParam("author") String author,
-    @FormParam("creation") String crea_date) {
-     return null;
+                            @FormParam("description") String description,
+                            @FormParam("keywords") String keywords,
+                            @FormParam("id") String id,
+                            @FormParam("crea_date") String crea_date) 
+ {    
+    String resultat = ""; 
+    Connection connection = null;
+       try {           
+           PreparedStatement statement;
+           String query;
+
+           Class.forName("org.apache.derby.jdbc.ClientDriver");
+           connection = DriverManager.getConnection("jdbc:derby://localhost:1527/pr2;user=pr2;password=pr2");  
+
+           try{
+               query = "UPDATE IMAGEN "
+                   + "SET TITULO = ?, DESCRIPCION = ?, PALABRAS_CLAVE = ?, FECHA_CREACION = ? "
+                   + "WHERE id = ?";   
+               statement = connection.prepareStatement(query);
+
+               statement.setString(1, title);
+               statement.setString(2, description); 
+               statement.setString(3, keywords);
+               statement.setString(4, crea_date);
+               statement.setString(5, id);
+
+               statement.executeUpdate();   
+               
+               resultat = "<h1> Imatge modificada </h1>";
+           }
+           catch(Exception e){
+               System.err.println(e.getMessage());
+               resultat = "<h1> Error </h1>";
+               return resultat;
+           }
+       } 
+       catch (Exception e) {
+           System.err.println(e.getMessage());
+       } 
+       finally {
+           try {
+               if (connection != null)
+                   connection.close();               
+           } 
+           catch (Exception e) {
+               // connection close failed.
+               System.err.println(e.getMessage());
+               resultat = "<h1> Error </h1>";
+               return resultat;
+           }
+       }
+       return resultat;
  }
  /**
  * GET method to list images
@@ -80,14 +192,203 @@ public class GenericResource {
  @Path("list")
  @GET
  @Produces(MediaType.TEXT_HTML)
- public String listImages () {
-     return null;
+ public String listImages () {   
+    String resultat = "<h1>Llista Imatges</1><br>";
+    Connection connection = null;
+    try{
+        String query;
+        PreparedStatement statement;
+        Class.forName("org.apache.derby.jdbc.ClientDriver");  
+
+        connection = DriverManager.getConnection("jdbc:derby://localhost:1527/pr2;user=pr2;password=pr2");
+
+        //Seleccion de todas las imagenes de la BD
+        query = "select * from imagen";
+        statement = connection.prepareStatement(query);
+        ResultSet rs = statement.executeQuery();
+
+        String autorImg = "null";  
+        resultat += "<table>";
+        
+            while(rs.next()){ 
+                   //Bucle para listar todas las imagenes y generar TABLA
+                   autorImg = rs.getString("AUTOR");
+                   String titulo = rs.getString("TITULO");
+                   resultat += "<tr><td>" + titulo  + "</td>";
+                   resultat += "<td><form action=\"/Practica4/modificarImagen.jsp\" method=\"POST\">";
+                   resultat += "<input type=\"submit\" value=\"Modificar Imagen\">";
+                   resultat += "<input type=\"hidden\" name=\"titulo\" value =\"" + rs.getString("TITULO") + "\"> ";
+                   resultat += "<input type=\"hidden\" name=\"descripcion\" value =\"" + rs.getString("DESCRIPCION") + "\">";
+                   resultat += "<input type=\"hidden\" name=\"palabras_clave\" value =\"" + rs.getString("PALABRAS_CLAVE") + "\">";
+                   resultat += "<input type=\"hidden\" name=\"fecha_creacion\" value =\"" + rs.getString("FECHA_CREACION") + "\">";
+                   resultat += "<input type=\"hidden\" name=\"id\" value =\"" + rs.getString("ID") + "\">";
+                   resultat += "</form></td>"; 
+                   resultat += "</tr>";
+            }  
+        resultat += "</table>";
+        resultat += "<form action=\"/Practica4/menu.jsp\" method=\"POST\" >";  
+        resultat += "<input type=\"submit\" value=\"Menu\">";              
+        resultat += "</form>"; 
+    }
+    catch(Exception e){
+        System.err.println(e.getMessage());
+    }
+    finally {
+        try {
+            if (connection != null) {
+                connection.close();
+            }
+        } 
+        catch (Exception e) {
+            // connection close failed.
+            System.err.println(e.getMessage());
+        }
+    }
+    return resultat;
  }
- /**
- * GET method to search images by id
- * @param id
- * @return
- */
- 
+
+/**
+    * POST method to search image
+    * @param title
+    * @param description
+    * @param keywords
+    * @param author
+    * @param crea_date
+    * @return
+    */
+@Path("searchCombo")
+@POST
+@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+@Produces(MediaType.TEXT_HTML)
+public String searchCombo (@FormParam("title") String title,
+                            @FormParam("author") String author,
+                            @FormParam("description") String description,
+                            @FormParam("keywords") String keywords,
+                            @FormParam("crea_date") String crea_date){
+    
+    Connection connection = null;        
+    String resultado = "";
+
+    int bool_autor = 0;
+    int bool_descrip = 0;
+    int bool_titulo = 0;
+    int bool_fecha = 0;
+    int bool_pal = 0;
+
+    String vpalclavebusqueda [] = keywords.split(";");
+
+    try {           
+        PreparedStatement statement,statement2;
+        String query,query2;
+
+        Class.forName("org.apache.derby.jdbc.ClientDriver");
+        connection = DriverManager.getConnection("jdbc:derby://localhost:1527/pr2;user=pr2;password=pr2"); 
+
+        try{
+            query = "SELECT * FROM imagen";
+            query2 = "select count(id) from imagen ";
+
+            //Comprobacion -> campo de datos vacio [0] o no [1]
+            if (author != null && !author.isEmpty())
+                bool_autor = 1;
+            if (description != null && !description.isEmpty())
+                bool_descrip = 1;
+            if (title != null && !title.isEmpty())
+                bool_titulo = 1;
+            if (crea_date != null && !crea_date.isEmpty())
+                bool_fecha = 1;
+            if (keywords != null && !keywords.isEmpty())
+                bool_pal = 1;
+
+            int num = -1;
+            int nume = 0; //esta variable es para saber si toca where o and
+            if (bool_autor == 1) {
+                if (nume == 0) query += " WHERE";
+                else query += " and";
+                nume ++;
+                query += " autor LIKE '"+author+"'";
+            }
+            if (bool_descrip == 1) {
+                if (nume == 0) query += " WHERE";
+                else query += " and";
+                nume ++;
+                query += " descripcion LIKE '"+description+"'";
+            }
+            if (bool_fecha == 1) {
+                 if (nume == 0) query += " WHERE";
+                else query += " and";
+                nume ++;
+                query += " fecha_creacion LIKE '"+crea_date+"'";
+            }
+            if (bool_titulo == 1) {
+                 if (nume == 0) query += " WHERE";
+                else query += " and";
+                nume ++;
+                query += " titulo LIKE '"+title+"'";
+            }
+            if (bool_pal == 1) {
+                for (int v = 0; v< vpalclavebusqueda.length ;v++) {
+                    if (nume == 0) query += " WHERE";
+                    else query += " and";
+                    nume ++;
+                    query += " palabras_clave LIKE '%"+vpalclavebusqueda[v]+"%'";
+                }
+                
+            }
+            statement = connection.prepareStatement(query);
+            statement2 = connection.prepareStatement(query2);
+
+            ResultSet rs = statement.executeQuery(); 
+            ResultSet rs2 = statement2.executeQuery(); 
+            
+            resultado += "<h1> Imagenes encontradas </h1>"; 
+                    resultado += "<table style=\"width:100%\"> "
+                            + "<tr>"
+                            + "<th>Id</th> <th>Titulo</th> <th>Descripcion</th> <th>Autor</th> <th>Fecha Creación</th> <th>Fecha_Alta</th> <th>Nombre Fichero</th> <th>Modificar Foto</th> <th>Eliminar Foto</th>"
+                            + "</tr> ";
+                    
+            if (rs2.next()) {
+                //Numero de imagenes en la BD
+                num = rs2.getInt(1);
+                if(nume == 0)
+                    num = 0; //Si no se busca ningun campo -> no sale ninguna imagen
+            }
+            if (num > 0){
+                while (rs.next()) {
+                    int idf = rs.getInt("id");
+                    String titulof = rs.getString("titulo");
+                    String autorf = rs.getString("autor");
+                    String descirpcionf = rs.getString("descripcion");
+                    String fechaf = rs.getString("fecha_creacion");
+                    String nomf = rs.getString("nombre_fichero");
+                    String fechaa = rs.getString("fecha_alta");                  
+                    
+                    resultado += "<tr> "
+                       + "<td> "+idf+" </td> <td>"+titulof+"</td> <td>"+descirpcionf+"</td> <td>"+autorf+"</td> <td>"+fechaf+"</td> <td>"+fechaa+"</td> <td>"+nomf+"</td>";                                   
+                }
+            }
+        }    
+        catch(Exception e){
+            System.err.println(e.getMessage());
+            resultado = "<h1> Error </h1>";
+        }            
+
+    }
+    catch (Exception e) {
+        System.err.println(e.getMessage());
+    } 
+    finally {
+        try {
+            if (connection != null)
+                connection.close();               
+        } 
+        catch (Exception e) {
+            // connection close failed.
+            System.err.println(e.getMessage());
+            resultado = "<h1> Error </h1>";
+        }
+    }
+    return resultado;
+} 
         
 }
